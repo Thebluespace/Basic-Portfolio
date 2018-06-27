22f3026cd8babe77815b28f6608df9d27ad88f48
Thomas Greene <tecgreene93@gmail.com>
2018-06-27 19:10:35 -0400

    changed page links from anchors to buttons with js functions

---
+++ assets/main.js
@@ -0,0 +1,9 @@
+function indexButton() {
+    window.location.href="index.html";
+}
+function contactButton() {
+    window.location.href="contact.html";
+}
+function portfolioButton() {
+    window.location.href="portfolio.html";
+}
