d89ceed7b8cd697bf224266de2773ff22de5bfb0
Thomas Greene <tecgreene93@gmail.com>
2018-06-27 23:44:53 -0400

    portfolio pages mostly complete

--- assets/main.js
+++ assets/main.js
@@ -7,3 +7,19 @@ function contactButton() {
 function portfolioButton() {
     window.location.href="portfolio.html";
 }
+
+function pbutton1() {
+    window.location.href="https://www.google.com/";
+}
+function pbutton2() {
+    window.location.href="https://www.google.com/";
+}
+function pbutton3() {
+    window.location.href="https://www.google.com/";
+}
+function pbutton4() {
+    window.location.href="https://www.google.com/";
+}
+function pbutton5() {
+    window.location.href="https://www.google.com/";
+}
